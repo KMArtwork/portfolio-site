36db4eeeceef92be86cea696599fc70d02dcda66
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-07-12 05:05:13 -0700

    resolving sass issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.scss';
+import './style/style.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
