cc964e382d827d2daf89aec55fbad1093c3e37d6
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-07-12 05:08:25 -0700

    learning sass source maps

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import './style/App.scss';
+// import './style/style.css';
 
 function App() {
   return (
